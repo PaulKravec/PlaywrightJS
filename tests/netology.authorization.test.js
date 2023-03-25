5afa72d78772cdef4f4fa66a052d9e29d878878b
Paul_KrVc <paul1312white@gmail.com>
2023-03-25 21:11:01 +0300

    new test

--- tests/netology.authorization.test.js
+++ tests/netology.authorization.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-const user = require('../user');
+const Value = require('../user.js');
 const invalidValues = { 
     mail: "paul13@gmail.com", 
     pass: "qwerty"
@@ -7,12 +7,23 @@ const invalidValues = {
 
 
 test('Succesfuly authorization', async ({ page }) => {
-  await page.goto('https://netology.ru/?modal=sign_in');
+  await page.goto('https://netology.ru/?modal=sign_in', {timeout: 100000});
   await expect(page.getByPlaceholder('Email')).toBeEnabled();
   await page.getByPlaceholder('Email').click();
-  await page.getByPlaceholder('Email').fill(user.mail);
+  await page.getByPlaceholder('Email').fill(Value.mail);
   await page.getByPlaceholder('Пароль').click();
-  await page.getByPlaceholder('Пароль').fill(user.password);
+  await page.getByPlaceholder('Пароль').fill(Value.password);
   await page.getByTestId('login-submit-btn').click();
   await expect(page.getByText('heading', { name: 'Мои курсы и профессии' })).toBeVisible();
+});
+
+test('Invaild values', async ({page}) => {
+    await page.goto('https://netology.ru/?modal=sign_in', {timeout: 100000});
+    await expect(page.getByPlaceholder('Email')).toBeEnabled();
+    await page.getByPlaceholder('Email').click();
+    await page.getByPlaceholder('Email').fill(invalidValues.mail);
+    await page.getByPlaceholder('Пароль').click();
+    await page.getByPlaceholder('Пароль').fill(invalidValues.password);
+    await page.getByTestId('login-submit-btn').click();
+    await expect(page.getByText("text=Вы ввели неправильно логин или пароль")).toBeVisible;
 });
